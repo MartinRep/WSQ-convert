2094d119a327e101fa99abc6d1aa670e9c904c21
martin.repicky <martin.repicky@novanta.com>
2023-10-01 17:13:26 +0200

    Refactoring

--- wsq_convert/GUIrunner.java
+++ wsq_convert/GUIrunner.java
@@ -1,110 +1,113 @@
 package wsq_convert;
 
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.awt.*;
+import java.util.List;
 
 public class GUIrunner extends JPanel {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -8473377041942261003L;
-	JButton folderButton;
-	JFileChooser chooser;
-	JButton convertButton;
-	WsqConvert wsqConvert;
-	File folderSelected;
-	JLabel descLabel;
-	JLabel infoLabel;
 
-	public GUIrunner() {
-		setLayout(new GridBagLayout());
-		GridBagConstraints constraints = new GridBagConstraints();
-		
-		descLabel = new JLabel();
-		descLabel.setForeground(Color.BLACK);
-		descLabel.setFont(new Font("Verdana", Font.PLAIN, 12));
-		labelPrintf("<b>1.</b> Vyber adresár s WSQ súbormi.", descLabel, true);
-		labelPrintf("<b>2.</b> Program vypíše koľko súborov bude spracovaných.", descLabel, false);
-		labelPrintf("<b>3.</b> Stlač CONVERT", descLabel, false);
-		labelPrintf("<b>4.</b> Ku každému WSQ súboru sa vytvorý PNG súbor", descLabel, false);
-		labelPrintf("<br/>Poznámka:", descLabel, false);
-		labelPrintf("Z vybraného adresára sa spracujú súbory aj z podadresárov<br/>", descLabel, false);
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		constraints.gridwidth = 2;
-		constraints.insets = new Insets(5, 5, 5, 5);
-		constraints.anchor = GridBagConstraints.NORTH;
-		add(descLabel, constraints);
-		
-		folderButton = new JButton("Vyber adresar s WSQ súbormi...");
-		folderButton.addActionListener((ActionEvent e) -> {
-			chooser = new JFileChooser();
-			chooser.setCurrentDirectory(new java.io.File("."));
-			chooser.setDialogTitle("Vyber adresar s WSQ súbormi...");
-			chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-			chooser.setAcceptAllFileFilterUsed(false);
-			if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-				folderSelected = chooser.getSelectedFile();
-				ArrayList<String> wsqFiles = new ArrayList<String>();
-				WsqConvert.traverseDirectory(".*\\.wsq", chooser.getSelectedFile(), wsqFiles);
-				labelPrintf(String.format("Počet vybraných súborov: %d", wsqFiles.size()), infoLabel, true);
-				convertButton.setVisible(true);
-			}
-		});
-		constraints.gridx = 0;
-		constraints.gridy = 1;
-		add(folderButton, constraints);
-		
-		infoLabel = new JLabel();
-		infoLabel.setForeground(Color.BLACK);
-		infoLabel.setFont(new Font("Verdana", Font.BOLD, 15) );
-		constraints.gridx = 0;
-		constraints.gridy = 2;
-		add(infoLabel, constraints);
-		
-		convertButton = new JButton("Konvertovať");
-		convertButton.setVisible(false);
-		convertButton.addActionListener((ActionEvent e) -> {
-			try {
-				ArrayList<String> filesProcessed = (ArrayList<String>) WsqConvert.convertImages(folderSelected);
-				labelPrintf(String.format("Spracovaných súborov: %d", filesProcessed.size()), infoLabel, false);
-				convertButton.setVisible(false);
-			} catch (IOException e1) {
-				infoLabel.setForeground(Color.RED);
-				infoLabel.setFont(new Font("Verdana", Font.BOLD, 8));
-				infoLabel.setText(e1.getMessage());
-			}
-		});
-		constraints.gridx = 0;
-		constraints.gridy = 3;
-		add(convertButton, constraints);
+    private static final long serialVersionUID = -8473377041942261003L;
+    private JButton folderButton;
+    private JFileChooser chooser;
+    private JButton convertButton;
+    private File folderSelected;
+    private JLabel infoLabel;
 
-	}
-	
-	private void labelPrintf(String message, JLabel label, boolean newLine) {
-		String previousText = label.getText();
-		if(newLine == true) {
-			label.setText(String.format("<html><i>%s</i><html>", message));
-		} else {
-			previousText = previousText.substring(0, previousText.lastIndexOf("</i>"));
-			label.setText(String.format("%s<br>%s</i><html>", previousText, message));
-		}
-	}
+    public GUIrunner() {
+        initializeGUI();
+    }
 
-	public static void main(String s[]) {
-		JFrame frame = new JFrame("WSQ CONVERT");
-		GUIrunner panel = new GUIrunner();
-		frame.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				System.exit(0);
-			}
-		});
-		frame.getContentPane().add(panel, "North");
-		frame.setSize(new Dimension(400, 400));
-		frame.setVisible(true);
-	}
+    private void initializeGUI() {
+        setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        JLabel descLabel = createDescriptionLabel();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        add(descLabel, constraints);
+
+        folderButton = createFolderButton();
+        constraints.gridy = 1;
+        add(folderButton, constraints);
+
+        infoLabel = createInfoLabel();
+        constraints.gridy = 2;
+        add(infoLabel, constraints);
+
+        convertButton = createConvertButton();
+        constraints.gridy = 3;
+        add(convertButton, constraints);
+    }
+
+    private JLabel createDescriptionLabel() {
+        JLabel label = new JLabel();
+        label.setFont(new Font("Verdana", Font.PLAIN, 12));
+        label.setText("<html><i>1. Select WSQ directory.<br/>2. Number of files will be displayed.<br/>3. Press CONVERT.<br/>4. PNG files will be created for each WSQ.</i></html>");
+        return label;
+    }
+
+    private JButton createFolderButton() {
+        JButton button = new JButton("Select WSQ Directory...");
+        button.addActionListener(this::folderButtonClicked);
+        return button;
+    }
+
+    private void folderButtonClicked(ActionEvent e) {
+        chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new File("."));
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            folderSelected = chooser.getSelectedFile();
+            try {
+                List<String> wsqFiles = WsqConvert.fetchFilePathsMatchingPattern(".*\\.wsq", folderSelected);
+                infoLabel.setText(String.format("Total files selected: %d", wsqFiles.size()));
+                convertButton.setVisible(true);
+            } catch (Exception ex) {
+                infoLabel.setText("An error occurred.");
+            }
+        }
+    }
+
+    private JLabel createInfoLabel() {
+        JLabel label = new JLabel();
+        label.setFont(new Font("Verdana", Font.BOLD, 15));
+        return label;
+    }
+
+    private JButton createConvertButton() {
+        JButton button = new JButton("Convert");
+        button.setVisible(false);
+        button.addActionListener(this::convertButtonClicked);
+        return button;
+    }
+
+    private void convertButtonClicked(ActionEvent e) {
+        try {
+            List<String> filesProcessed = WsqConvert.convertImagesToPng(folderSelected);
+            infoLabel.setText(String.format("Processed files: %d", filesProcessed.size()));
+            convertButton.setVisible(false);
+        } catch (IOException e1) {
+            infoLabel.setText("An error occurred.");
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("WSQ CONVERT");
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        frame.getContentPane().add(new GUIrunner(), BorderLayout.NORTH);
+        frame.setSize(new Dimension(400, 400));
+        frame.setVisible(true);
+    }
 }
--- wsq_convert/WsqConvert.java
+++ wsq_convert/WsqConvert.java
@@ -1,83 +1,73 @@
 package wsq_convert;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.imageio.ImageIO;
-
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jnbis.Bitmap;
 import org.jnbis.WSQDecoder;
 
+public class WsqConvert {
 
+    public static BufferedImage bitmapToBufferedImage(Bitmap bitmap) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        byte[] data = bitmap.getPixels();
 
-class WsqConvert {
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        WritableRaster raster = image.getRaster();
+        raster.setDataElements(0, 0, width, height, data);
 
-	public static BufferedImage convert(Bitmap bitmap) {
-		int width = bitmap.getWidth();
-		int height = bitmap.getHeight();
-		byte[] data = bitmap.getPixels();
-		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
-		WritableRaster raster = image.getRaster();
-		raster.setDataElements(0, 0, width, height, data);
-		return image;
-	}
-	
-	
-	public static BufferedImage readImage(String fileName) throws FileNotFoundException, IOException {
-		File file = new File(fileName);
-		Bitmap bitmap = WSQDecoder.decode(new FileInputStream(file));
-		BufferedImage image = convert(bitmap);
-		return image;
-		
-	}
-	
-	public static void traverseDirectory(final String pattern, final File folder, List<String> fileNamesList) {
-        for (final File f : folder.listFiles()) {
+        return image;
+    }
 
-            if (f.isDirectory()) {
-                traverseDirectory(pattern, f, fileNamesList);
+    public static BufferedImage readBitmapImage(String fileName) throws IOException {
+        FileInputStream stream = new FileInputStream(new File(fileName));
+        Bitmap bitmap = WSQDecoder.decode(stream);
+        return bitmapToBufferedImage(bitmap);
+    }
+
+    public static List<String> fetchFilePathsMatchingPattern(final String pattern, final File folder) {
+        List<String> filePaths = new ArrayList<>();
+        for (File file : folder.listFiles()) {
+            if (file.isDirectory()) {
+                filePaths.addAll(fetchFilePathsMatchingPattern(pattern, file));
             }
-
-            if (f.isFile()) {
-                if (f.getName().matches(pattern)) {
-                    fileNamesList.add(f.getAbsolutePath());
-                }
+            if (file.isFile() && file.getName().matches(pattern)) {
+                filePaths.add(file.getAbsolutePath());
             }
         }
-	}
-	
-	public static List<BufferedImage> readImages(final File folder) throws FileNotFoundException, IOException{
-		List<String> fileNamesList = new ArrayList<String>();
-		List<BufferedImage> images = new ArrayList<BufferedImage>();
-		traverseDirectory(".*\\.wsq", folder, fileNamesList);
-		for (String fileName : fileNamesList) {
-			images.add(readImage(fileName));
-		}
-		return images;
-	}
-	
-	
-	public static List<String> convertImages(final File folder) throws IOException{
-		List<String> fileNamesList = new ArrayList<String>();
-		traverseDirectory(".*\\.wsq", folder, fileNamesList);
-		for (String fileName : fileNamesList) {
-			BufferedImage image = readImage(fileName);
-			int pos = fileName.lastIndexOf(".");
-			if (pos > 0 && pos < (fileName.length() - 1)) { // If '.' is not the first or last character.
-			    fileName = fileName.substring(0, pos);
-			    fileName = fileName.concat(".png");
-				File outputFile = new File(fileName);
-				ImageIO.write(image, "png", outputFile);
-//				System.out.printf("File: %s was saved.\n", outputFile);	
-			}
-		}
-		return fileNamesList;
-	}
+        return filePaths;
+    }
+
+    public static List<BufferedImage> readMultipleImages(final File folder) throws IOException {
+        List<String> filePaths = fetchFilePathsMatchingPattern(".*\\.wsq", folder);
+        List<BufferedImage> images = new ArrayList<>();
+
+        for (String filePath : filePaths) {
+            images.add(readBitmapImage(filePath));
+        }
+
+        return images;
+    }
+
+    public static List<String> convertImagesToPng(final File folder) throws IOException {
+        List<String> convertedFiles = new ArrayList<>();
+        List<String> filePaths = fetchFilePathsMatchingPattern(".*\\.wsq", folder);
+
+        for (String filePath : filePaths) {
+            BufferedImage image = readBitmapImage(filePath);
+            String outputFilePath = filePath.replaceAll("\\.wsq$", ".png");
+            File outputFile = new File(outputFilePath);
+            ImageIO.write(image, "png", outputFile);
+            convertedFiles.add(outputFilePath);
+        }
+
+        return convertedFiles;
+    }
 }
